c62b232f525242ad1bc09932f6faf13c8ede86cb
Shivam Baldha <67622526+shivambaldha@users.noreply.github.com>
2023-11-26 12:23:49 +0530

    code

---
+++ 02Variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This File is all About the Variables")
+
+	// to declare the variables use var keyword argument and then datatype and values
+	var name string = "Shivam B"
+	fmt.Println(name)  // USE  prinln to print the
+
+	var number int = 9723404352
+	fmt.Println(number)
+}
